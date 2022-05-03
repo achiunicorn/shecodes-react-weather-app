e551282efcf473a1deba36ea83244ed93e4d495f
Amanda Chiu <87955310+achiunicorn@users.noreply.github.com>
2022-05-03 13:48:51 -0400

    Added GitHub url in footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        Coded by Amanda Chiu.{" "}
+        <a
+          href="https://github.com/achiunicorn/shecodes-react-weather-app"
+          target="_blank"
+        >
+          Open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
